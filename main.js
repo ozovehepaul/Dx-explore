ae98378e9d3deacc9787833dbf79cb139fb1a9a2
ozovehepaul <114989613+ozovehepaul@users.noreply.github.com>
2022-10-29 15:57:53 +0100

    completed DX-Explore project

---
+++ main.js
@@ -0,0 +1,27 @@
+// console.log("send me some water")
+
+// var writeup = "i need water"
+
+// header section starts here
+console.log(document)
+
+var hambugerdiv = document.querySelector(".hambuger")
+var mobilelink = document.querySelector(".mobile-link-holder")
+var mybackdrop = document.querySelector(".backdrop")
+
+function dosomething() {
+    hambugerdiv.classList.toggle("change-buger")
+    mobilelink.classList.toggle("show-mobile-link-holder")
+    mybackdrop.classList.toggle("show-backdrop")
+    
+}
+
+// header section ends here
+
+// default settting starts here
+
+
+    
+
+console.log(hambugerdiv)
+
